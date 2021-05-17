84d3ae04a5f8295cf74b46b93f7dcea61ef7ef51
jyoti <jyotik@eiysys.in>
2021-05-17 14:57:57 +0530

    40

--- src/components/Dropdown/types.ts
+++ src/components/Dropdown/types.ts
@@ -1,7 +1,11 @@
 export type Position = "top" | "top-right" | "bottom";
+export type Top = "50px";
+export type Height= "500px";
 
 export interface PositionProps {
   position?: Position;
+  top?: Top;
+  height? : Height;
 }
 
 export interface DropdownProps extends PositionProps {
