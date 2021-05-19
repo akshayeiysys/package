7e75499f8c5b1b4714e075cb7bdfa8b65636cd94
jyoti <jyotik@eiysys.in>
2021-05-19 13:56:02 +0530

    58

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -4034,9 +4034,9 @@ var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { sec
     // secondary: "#f15e7a",
     background: "#100C18", backgroundDisabled: "#3c3742", backgroundAlt: "#27262c", contrast: "#FFFFFF", dropdown: "#1E1D20", invertedContrast: "#191326", input: "#483f5a", 
     // inputSecondary: "#66578D",
-    inputSecondary: "#f15e7a", primaryDark: "#0098A1", tertiary: "#353547", text: "#EAE2FC", textDisabled: "#666171", 
-    // textSubtle: "#A28BD4",
-    textSubtle: "#6400FF", borderColor: "#524B63", gradients: {
+    inputSecondary: "#f15e7a", primaryDark: "#0098A1", tertiary: "#353547", text: "#EAE2FC", textDisabled: "#666171", textSubtle: "#A28BD4", 
+    // textSubtle: "#6400FF",
+    borderColor: "#524B63", gradients: {
         bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
         cardHeader: "linear-gradient(166.77deg, #3B4155 0%, #3A3045 100%)",
         blue: "linear-gradient(180deg, #00707F 0%, #19778C 100%)",
